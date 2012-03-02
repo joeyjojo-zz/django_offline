41092795f5a4c74134045cc4f6bca7d753bdaadc
Jon Dunleavy <admin@defmyfunc.com>
2012-03-02 14:29:39 +0000

    added necessary models for and added login template that I am not sure I will need

--- src/example/main.py
+++ src/example/main.py
@@ -9,7 +9,7 @@ import wrapper.main
 
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-    wrapper.main.start("http://127.0.0.1:8000/admin", settings.getfullpathtodb())
+    wrapper.main.start("http://127.0.0.1:8000/home", settings.getfullpathtodb())
     sys.exit()
 
 
