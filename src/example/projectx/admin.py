21c1506581c2df2eb3eb04d81821e9ab426759f2
jond <joeyjojosmith@gmail.com>
2012-08-03 00:29:15 +0100

    Deleting all old content

--- src/example/projectx/admin.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'jond'
-
-from projectx.models import Product
-from django.contrib import admin
-
-admin.site.register(Product)
--- src/example/projectx/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-from django.views.generic import TemplateView
-
-
-class HomeView(TemplateView):
-    """
-    The projectX homepage, that users see when they start up the application
-    """
-    template_name = "home.html"
