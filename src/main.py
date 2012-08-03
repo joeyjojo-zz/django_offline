e0094e559ac3a72caf70e657910fe00502a0343c
jond <joeyjojosmith@gmail.com>
2012-08-03 14:54:18 +0100

    bit of cleanup

--- src/main.py
+++ src/main.py
@@ -10,12 +10,11 @@ if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     # then do django related importas
     import django_offline.frame
-    # start up the model
-    #setup_all(True)
-    #create_all()
     # start up qt
     app = QtGui.QApplication(sys.argv)
+    # allow debugging via pdb
     QtCore.pyqtRemoveInputHook()
+    # setup the window
     win = django_offline.frame.MainWindow()
     win.show()
     u = QtCore.QUrl('http://127.0.0.1:8000/polls/')
