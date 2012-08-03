e1c86c4681123e67fb2d24883a7b6c3827c1981d
jond <joeyjojosmith@gmail.com>
2012-08-03 14:48:03 +0100

    added basic poll viewz

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+from polls.models import Poll
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
