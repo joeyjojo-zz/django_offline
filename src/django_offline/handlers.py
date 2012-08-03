a143002b14627b49b88bd51b3489c9b986d20bf4
jond <joeyjojosmith@gmail.com>
2012-08-03 13:34:30 +0100

    fixed error where only part of the data was getting transferred and so the whole screen was not rendering.

--- src/django_offline/handlers.py
+++ src/django_offline/handlers.py
@@ -2,25 +2,6 @@ __author__ = 'jond'
 
 from PyQt4 import QtCore, QtNetwork
 
-import django.core.handlers.base
-
-class FakeHandler(django.core.handlers.base.BaseHandler):
-    def __init__(self):
-        django.core.handlers.base.BaseHandler.__init__(self)
-        # Set up middleware if needed. We couldn't do this earlier, because
-        # settings weren't available.
-        if self._request_middleware is None:
-            try:
-                # Check that middleware is still uninitialised.
-                if self._request_middleware is None:
-                    self.load_middleware()
-            except:
-                # Unload whatever middleware we got
-                self._request_middleware = None
-                raise
-
-
-
 class FakeReply(QtNetwork.QNetworkReply):
     """
     The reply class that is used when a url is to be dealt with by the application
@@ -32,25 +13,24 @@ class FakeReply(QtNetwork.QNetworkReply):
         """
         QtNetwork.QNetworkReply.__init__(self, parent)
         self.setRequest(request)
-        self.setUrl(request.url())
         self.setOperation(operation)
-        self.open(self.ReadOnly | self.Unbuffered)
-        # if any are lists of 1 item then just send the item through
+
         self.content = dj_response.content
         self.offset = 0
 
         self.setHeader(QtNetwork.QNetworkRequest.ContentTypeHeader, dj_response['Content-Type'])
-        #self.setHeader(QtNetwork.QNetworkRequest.ContentLengthHeader, len(self.content))
+        self.setHeader(QtNetwork.QNetworkRequest.ContentLengthHeader, len(self.content))
 
         QtCore.QTimer.singleShot(0, self, QtCore.SIGNAL("readyRead()"))
         QtCore.QTimer.singleShot(0, self, QtCore.SIGNAL("finished()"))
-
+        self.open(self.ReadOnly | self.Unbuffered)
+        self.setUrl(request.url())
 
     def abort(self):
         pass
 
     def bytesAvailable(self):
-        return len(self.content) - self.offset
+        return (len(self.content) - self.offset) + QtCore.QIODevice.bytesAvailable(self)
 
     def isSequential(self):
         return True
