2feb33d659efeb11f62b54a4fd6fad4a553d0a1e
jond <joeyjojosmith@gmail.com>
2012-08-04 20:37:14 +0100

    finishing work completing django tutorial as an example of how to use django-offline

--- src/polls/admin.py
+++ src/polls/admin.py
@@ -1,6 +1,7 @@
 __author__ = 'Jon'
 
-from polls.models import Poll
+from polls.models import Poll, Choice
 from django.contrib import admin
 
 admin.site.register(Poll)
+admin.site.register(Choice)
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,14 +1,15 @@
 # Create your views here.
 from polls.models import Poll
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
