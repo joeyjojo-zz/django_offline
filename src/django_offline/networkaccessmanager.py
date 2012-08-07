6cd20b8fccee231b463159abb4f8b98067834e61
jond <joeyjojosmith@gmail.com>
2012-08-08 00:41:25 +0100

    some work on getting the admin module to work correctly.

--- src/django_offline/networkaccessmanager.py
+++ src/django_offline/networkaccessmanager.py
@@ -23,27 +23,30 @@ class NetworkAccessManager(QtNetwork.QNetworkAccessManager):
         Deal with the request when it comes in
         """
         argd = {}
-        # lets check that data is a dictionary
-        if data is not None:
-            postargs = unicode(data.readAll())
-            argd = urlparse.parse_qs(urllib.unquote_plus(postargs.encode('ascii')).decode('utf-8'),
-                                                         keep_blank_values=True)
-        # Set up the handler
-        from django.core.wsgi import get_wsgi_application
-        handler = StaticFilesHandler(get_wsgi_application())
-        # Convert the request to a django request
-        dj_request = None
-        rf = django.test.client.RequestFactory()
-        urlstring = unicode(request.url().toString())
-        if operation == QtNetwork.QNetworkAccessManager.PostOperation:
-            dj_request = rf.post(urlstring, argd)
-        elif operation == QtNetwork.QNetworkAccessManager.GetOperation:
-            dj_request = rf.get(urlstring, argd)
-        # add the response from django to the reply
-        response = handler(dj_request.environ, HttpResponse)
-        reply = django_offline.handlers.FakeReply(self, request, operation, response)
-        # set up the reply with the correct status
-        #reply.setAttribute(QtNetwork.QNetworkRequest.HttpStatusCodeAttribute, 200)
+        reply = None
+        if request.url().host() == '127.0.0.1':
+            # lets check that data is a dictionary
+            if data is not None:
+                postargs = unicode(data.readAll())
+                argd = urlparse.parse_qs(urllib.unquote_plus(postargs.encode('ascii')).decode('utf-8'),
+                                                             keep_blank_values=True)
+            # Set up the handler
+            from django.core.wsgi import get_wsgi_application
+            handler = StaticFilesHandler(get_wsgi_application())
+            # Convert the request to a django request
+            dj_request = None
+            rf = django.test.client.RequestFactory()
+            urlstring = unicode(request.url().toString())
+            if operation == QtNetwork.QNetworkAccessManager.PostOperation:
+                dj_request = rf.post(urlstring, argd)
+            elif operation == QtNetwork.QNetworkAccessManager.GetOperation:
+                dj_request = rf.get(urlstring, argd)
+            # add the response from django to the reply
+            response = handler(dj_request.environ, HttpResponse)
+            reply = django_offline.handlers.FakeReply(self, request, operation, response)
+            # set up the reply with the correct status
+            #reply.setAttribute(QtNetwork.QNetworkRequest.HttpStatusCodeAttribute, 200)
         if reply is None:
             reply = QtNetwork.QNetworkAccessManager.createRequest(self, operation, request, data)
+        reply.ignoreSslErrors()
         return reply
--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -96,7 +96,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'mysite.disable.DisableCSRF',
+    # 'mysite.disable.DisableCSRF',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
